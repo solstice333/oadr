f40e1621a0ee5722762b48cb221ebaeee7af885e
knavero <knavero@gmail.com>
2013-04-24 08:29:07 -0700

    edited run

--- oadr2-app/src/main/java/org/enernoc/open/oadr2/xmpp/OadrApp.java
+++ oadr2-app/src/main/java/org/enernoc/open/oadr2/xmpp/OadrApp.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import javax.xml.bind.JAXBException;
 import javax.xml.datatype.DatatypeConfigurationException;
 
+import org.enernoc.open.oadr2.model.OadrCreatedEvent;
 import org.enernoc.open.oadr2.model.OadrDistributeEvent;
 import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPConnection;
@@ -13,14 +14,13 @@ import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.IQ;
 import org.jivesoftware.smack.packet.Packet;
 
-
-//run as java application
 public class OadrApp {
    static String username;
    static String password;
    static boolean isVen;
    static ConnHandler connHandler = new ConnHandler();
    static XMPPConnection ven = null, vtn = null;
+   static final String OADR2_XMLNS = "http://openadr.org/oadr-2.0a/2012/07";
 
    public static void main(String[] args)
          throws DatatypeConfigurationException, FileNotFoundException,
@@ -136,19 +136,53 @@ public class OadrApp {
       else {
          venConnect();
 
-         // packet collector result queue
-         // PacketCollector packetCollector = ven
-         // .createPacketCollector(new OADR2PacketFilter());
-
-         PacketListener oadrListener = new PacketListener() {
+         //add listener to ven
+         PacketListener oadrDistributeEventListener = new PacketListener() {
             @Override
             public void processPacket(Packet packet) {
-               System.out.println("Printing packet...");
-               printPacket(packet);
+               // grab packet extension and typecast payload to OadrDistributeEvent
+               // object
+               OADR2PacketExtension oadrExtension = (OADR2PacketExtension) packet
+                     .getExtension(OADR2_XMLNS);
+               OadrDistributeEvent ode = (OadrDistributeEvent) oadrExtension
+                     .getPayload();
+
+               // parsing oadrDistributeEvent payload for relevant elements and
+               // instantiating OadrCreatedEvent payload via factory method
+               System.out.println("parsing oadrDistributeEvent payload...");
+               OadrCreatedEvent oce = new OadrPayloadFactory()
+                     .createResponsePayload(ode);
+
+               // print xml of OadrCreatedEvent payload
+               try {
+                  System.out.println("createdEventPayload: ");
+                  ConnHandler.testNamespace(oce);
+                  OADR2PacketExtension pe = new OADR2PacketExtension(oce,
+                        new JAXBManager());
+                  System.out.println("xmlns: " + pe.getNamespace());
+               }
+               catch (Exception e) {
+                  e.printStackTrace();
+               }
+               finally {
+                  System.out.println();
+               }
+
+               // creates IQ packet for oadrCreatedEvent and sends to vtn
+               System.out.println("sending oadrCreatedEvent payload...");
+               try {
+                  IQ oceiq = new ConnHandler().createIQ(packet.getFrom(), oce);
+                  ven.sendPacket(oceiq);
+               }
+               catch (Exception e) {
+                  e.printStackTrace();
+               }
+
+               System.out.println("oadrCreatedEvent payload sent!");
             }
          };
-
-         ven.addPacketListener(oadrListener, new OADR2PacketFilter());
+         ven.addPacketListener(oadrDistributeEventListener,
+               new OADR2PacketFilter());
 
          while (true) {
             System.out
